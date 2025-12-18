44f9270afadc3a94adc920d79bc5a5136e04125f
Raghav N. <raghavna37@gmail.com>
2025-12-18 10:27:43 -0500

    Refactor bottomPipe initialization for readability

--- PipePair.java
+++ PipePair.java
@@ -8,12 +8,7 @@ public class PipePair {
 
     public PipePair(int x, int topHeight, int width, int gap, int panelHeight) {
         topPipe = new Rectangle(x, 0, width, topHeight);
-        bottomPipe = new Rectangle(
-            x,
-            topHeight + gap,
-            width,
-            panelHeight - (topHeight + gap)
-        );
+        bottomPipe = new Rectangle(x, topHeight + gap, width, panelHeight - (topHeight + gap));
     }
 
     public void move(int speed) {
